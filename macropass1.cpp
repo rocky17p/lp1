af408f38457930543ee6131d3fe5273a1368fc7d
RISHI PATWA <rishi17p@gmail.com>
2025-10-26 19:33:38 +0530

    Update macropass1.cpp

--- macropass1.cpp
+++ macropass1.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct MNTEntry {
+struct MNTEntry
+{
     int index;
     string name;
     int pntabPtr;
@@ -9,14 +10,15 @@ struct MNTEntry {
     int mdtPtr;
 };
 
-int main() {
+int main()
+{
     ifstream fin("input2.txt");
     ofstream mntFile("MNT.txt"), mdtFile("MDT.txt"), pntabFile("PNTAB.txt"), kpdtabFile("KPDTAB.txt"), ic("IC.txt");
 
     vector<string> MDT;
     vector<MNTEntry> MNT;
     map<string, vector<string>> PNTABs;
-    vector<tuple<string, string, string>> KPDTAB;
+    vector<vector<string>> KPDTAB;
 
     int macroDepth = 0, macroIndex = 0, kpIndex = 1;
     bool headerNext = false;
@@ -25,11 +27,13 @@ int main() {
     map<string, int> paramIndex;
 
     string line;
-    while (getline(fin, line)) {
+    while (getline(fin, line))
+    {
         string L = line;
 
         // Detect MACRO
-        if (L.find("MACRO") != string::npos) {
+        if (L.find("MACRO") != string::npos)
+        {
             macroDepth++;
             if (macroDepth == 1)
                 headerNext = true;
@@ -39,7 +43,8 @@ int main() {
         }
 
         // Process macro header
-        if (headerNext) {
+        if (headerNext)
+        {
             stringstream ss(L);
             ss >> currentMacro;
 
@@ -55,9 +60,11 @@ int main() {
             string p;
             int idx = 1;
 
-            while (getline(ps, p, ',')) {
+            while (getline(ps, p, ','))
+            {
                 int eq = p.find('=');
-                if (eq != -1) {
+                if (eq != -1)
+                {
                     KPDTAB.push_back({currentMacro, p.substr(0, eq), p.substr(eq + 1)});
                     kpIndex++;
                 }
@@ -76,31 +83,37 @@ int main() {
         }
 
         // Inside macro body
-        if (macroDepth > 0) {
-            if (L.find("MEND") != string::npos) {
+        if (macroDepth > 0)
+        {
+            if (L.find("MEND") != string::npos)
+            {
                 MDT.push_back("MEND");
                 macroDepth--;
                 continue;
             }
 
-            for (auto &p : params) {
+            for (auto &p : params)
+            {
                 string rep = "#" + to_string(paramIndex[p]);
                 int pos = L.find(p);
-                while (pos != -1) {
+                while (pos != -1)
+                {
                     L.replace(pos, p.size(), rep);
                     pos = L.find(p);
                 }
             }
             MDT.push_back(L);
-        } 
-        else {
+        }
+        else
+        {
             ic << L << "\n";
         }
     }
 
     // Write MNT
     mntFile << "Index\tName\tPNTAB\tKPDTAB\tMDT\n";
-    for (auto &e : MNT) {
+    for (auto &e : MNT)
+    {
         mntFile << e.index << "\t" << e.name << "\t" << e.pntabPtr << "\t"
                 << e.kpdtPtr << "\t" << e.mdtPtr << "\n";
     }
@@ -110,7 +123,8 @@ int main() {
         mdtFile << i + 1 << "\t" << MDT[i] << "\n";
 
     // Write PNTAB
-    for (auto &kv : PNTABs) {
+    for (auto &kv : PNTABs)
+    {
         pntabFile << kv.first << ":\n";
         for (int i = 0; i < kv.second.size(); i++)
             pntabFile << i + 1 << "\t" << kv.second[i] << "\n";
@@ -118,8 +132,12 @@ int main() {
 
     // Write KPDTAB
     for (int i = 0; i < KPDTAB.size(); i++)
-        kpdtabFile << i + 1 << "\t" << get<0>(KPDTAB[i]) << "\t"
-                   << get<1>(KPDTAB[i]) << "\t" << get<2>(KPDTAB[i]) << "\n";
+    {
+        kpdtabFile << i + 1 << "\t"
+                   << KPDTAB[i][0] << "\t"  // Macro name
+                   << KPDTAB[i][1] << "\t"  // Parameter name
+                   << KPDTAB[i][2] << "\n"; // Default value
+    }
 
     cout << "Pass 1 Done!\n";
     return 0;

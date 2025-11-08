92a3d13e22bef70031c224ccddf0e077bfa103dc
RISHI PATWA <rishi17p@gmail.com>
2025-11-08 20:17:01 +0530

    Update pass2.cpp

--- pass2.cpp
+++ pass2.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-string pad3(const string &s)
+string pad3(string const &s)
 {
     string t = s;
     t.erase(remove(t.begin(), t.end(), ' '), t.end());
@@ -26,69 +26,65 @@ string pad3(const string &s)
     }
     return t;
 }
-
 int main()
 {
+    ifstream sc("symtable.txt");
+    if (!sc)
+    {
+        cout << "Unable to open symbol table file" << endl;
+        return 1;
+    }
     unordered_map<int, string> symaddr;
     {
-
-        ifstream st("symtable.txt");
-        if (!st)
-        {
-            cout << "unable to open symtable.txt" << endl;
-            return 1;
-        }
         int no;
-        string name, addr;
-        while (st >> no >> name >> addr)
+        string name, memo;
+        while (sc >> no >> name >> memo)
         {
-            symaddr[no] = addr;
+            symaddr[no] = memo;
         }
+    }
+    ifstream lt("littable.txt");
+    if (!lt)
+    {
+        cout << "Unable to open literal table file" << endl;
+        return 1;
     }
     unordered_map<int, string> litaddr;
     {
-
-        ifstream lt("littable.txt");
-        if (!lt)
-        {
-            cout << "unable to open littable.txt" << endl;
-            return 1;
-        }
         int no;
-        string name, addr;
-        while (lt >> no >> name >> addr)
+        string name, memo;
+        while (lt >> no >> name >> memo)
         {
-            litaddr[no] = addr;
+            litaddr[no] = memo;
         }
-    }
-    ofstream mc("machine_code.txt");
-    if (!mc)
-    {
-        cout << "failed to open machine_code" << endl;
-        return 1;
     }
     ifstream icin("ic.txt");
     if (!icin)
     {
-        cout << "failed to open ic.txt" << endl;
+        cout << "Unable to open intermediate code file" << endl;
+        return 1;
+    }
+    ofstream mc("machine_code.txt");
+    if (!mc)
+    {
+        cout << "Unable to open machine code file" << endl;
         return 1;
     }
     string line;
     while (getline(icin, line))
     {
-        if (line.empty())
-            continue;
         string lc, ic;
         stringstream ss(line);
         ss >> lc >> ic;
+
         vector<string> groups;
         string temp;
         for (int i = 0; i < ic.size(); i++)
         {
             if (ic[i] == '(')
             {
-                temp.clear();
                 i++;
+                temp.clear();
                 while (i < ic.size() && ic[i] != ')')
                 {
                     temp += ic[i];
@@ -101,59 +97,56 @@ int main()
         if (!groups.empty())
         {
             string g0 = groups[0];
-
             if (g0.substr(0, 3) == "IS,")
             {
                 string opcode = g0.substr(3);
-                string reg = "0", mem = "000";
+                string reg = "0";
+                string memo = "000";
                 if (opcode == "00")
                 {
-                    MC = opcode + "\t0\t000";
+                    MC = opcode + "\t" + reg + "\t" + memo;
                 }
                 else
                 {
                     int idx = 1;
-                    if (groups.size() > 1 && groups[1].find(",") == string::npos)
+                    if (groups.size() > 1 && groups[idx].find(",") == string::npos)
                     {
-                        reg = groups[1];
-                        idx = 2;
+                        reg = groups[idx];
+                        idx++;
                     }
-
                     if (groups.size() > idx)
                     {
-                        string opg = groups[idx];
-                        if (opg.substr(0, 2) == "S,")
+                        string g2 = groups[idx];
+                        if (g2.substr(0, 2) == "S,")
                         {
-                            string idstr = opg.substr(2);
-                            int id = (idstr.empty()) ? -1 : stoi(idstr);
-                            mem = symaddr.count(id) ? pad3(symaddr[id]) : "000";
+                            string idstr = g2.substr(2);
+                            int id = idstr.empty() ? -1 : stoi(idstr);
+                            memo = symaddr.count(id) ? pad3(symaddr[id]) : "000";
                         }
-                        else if (opg.substr(0, 2) == "L,")
+                        else if (g2.substr(0, 2) == "L,")
                         {
-                            string idstr = opg.substr(2);
-                            int id = (idstr.empty()) ? -1 : stoi(idstr);
-                            mem = litaddr.count(id) ? pad3(litaddr[id]) : "000";
+                            string idstr = g2.substr(2);
+                            int id = idstr.empty() ? -1 : stoi(idstr);
+                            memo = litaddr.count(id) ? pad3(litaddr[id]) : "000";
                         }
-                        else if (opg.substr(0, 2) == "C,")
+                        else if (g2.substr(0, 2) == "C,")
                         {
-                            string val = opg.substr(2);
-                            mem = val;
+                            memo = g2.substr(2);
                         }
                     }
+                    MC = opcode + "\t" + reg + "\t" + memo;
                 }
-                MC = opcode + "\t" + reg + "\t" + mem;
             }
             else if (g0.substr(0, 3) == "DL,")
             {
-                string dl = g0.substr(3);
-                if (dl == "01")
+                if (g0.substr(3) == "01")
                 {
-                    string val = "0";
-                    if (groups.size() >= 2 && groups[1].substr(0, 2) == "C,")
+                    string val = "000";
+                    if (groups.size() > 1 && groups[1].substr(0, 2) == "C,")
                     {
-                        val = groups[1].substr(2);
+                        string val = groups[1].substr(2);
+                        MC = "00\t0\t" + pad3(val);
                     }
-                    MC = "00\t0\t" + pad3(val);
                 }
                 else
                 {
@@ -167,5 +160,6 @@ int main()
         }
         mc << lc << "\t" << MC << endl;
     }
+    cout << "Pass 2 done" << endl;
     return 0;
 }

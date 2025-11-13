48b93bc86ca7407227d99133ec1c0181baf67493
RISHI PATWA <rishi17p@gmail.com>
2025-11-13 22:38:38 +0530

    Update pass1.cpp

--- pass1.cpp
+++ pass1.cpp
@@ -3,260 +3,275 @@ using namespace std;
 
 struct OP
 {
-    string opcode, mclass, mnemonic;
+    string opcode;
+    string mclass;
+    string mnemonic;
 };
-struct Sym
+
+struct sym
 {
     int no;
     string name, addr;
 };
-struct Lit
+struct lit
 {
     int no;
     string name, addr;
 };
-struct Pool
+struct pool
 {
     int no;
-    string litno;
+    string literalno;
 };
 
-OP optab[18] = {
-    {"STOP", "IS", "00"}, {"ADD", "IS", "01"}, {"SUB", "IS", "02"}, {"MULT", "IS", "03"}, {"MOVER", "IS", "04"}, {"MOVEM", "IS", "05"}, {"COMP", "IS", "06"}, {"BC", "IS", "07"}, {"DIV", "IS", "08"}, {"READ", "IS", "09"}, {"PRINT", "IS", "10"}, {"START", "AD", "01"}, {"END", "AD", "02"}, {"ORIGIN", "AD", "03"}, {"EQU", "AD", "04"}, {"LTORG", "AD", "05"}, {"DC", "DL", "01"}, {"DS", "DL", "02"}};
+OP optab[18] = {{"STOP", "IS", "00"}, {"ADD", "IS", "01"}, {"SUB", "IS", "02"}, {"MULT", "IS", "03"}, {"MOVER", "IS", "04"}, {"MOVEM", "IS", "05"}, {"COMP", "IS", "06"}, {"BC", "IS", "07"}, {"DIV", "IS", "08"}, {"READ", "IS", "09"}, {"PRINT", "IS", "10"}, {"START", "AD", "01"}, {"END", "AD", "02"}, {"ORIGIN", "AD", "03"}, {"EQU", "AD", "04"}, {"LTORG", "AD", "05"}, {"DS", "DL", "02"}, {"DC", "DL", "01"}};
 
-Sym ST[50];
-Lit LT[50];
-Pool PT[50];
-
-int scnt = 0, lcnt = 0, nlcnt = 0, pcnt = 0;
-int poolStartIndex = 0;
-
-unordered_map<string, int> reg = {
-    {"AREG", 1}, {"BREG", 2}, {"CREG", 3}, {"DREG", 4}};
-unordered_map<string, int> cond = {
-    {"LT", 1}, {"LE", 2}, {"EQ", 3}, {"GT", 4}, {"GE", 5}, {"ANY", 6}};
-
-int getOP(string s)
+int scount = 0, lcount = 0, nlcount = 0, pcount = 0, poolstartindex = 0;
+sym ST[50];
+lit LT[50];
+pool PT[50];
+int getop(string s)
 {
     for (int i = 0; i < 18; i++)
+    {
         if (optab[i].opcode == s)
+        {
             return i;
+        }
+    }
     return -1;
 }
-
+int getsymid(string s)
+{
+    for (int i = 0; i < scount; i++)
+    {
+        if (ST[i].name == s)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 bool presentST(string s)
 {
-    for (int i = 0; i < scnt; i++)
+    for (int i = 0; i < scount; i++)
+    {
         if (ST[i].name == s)
+        {
             return true;
+        }
+    }
+    return false;
+}
+bool presentcurrentpool(string s)
+{
+    for (int i = poolstartindex; i < lcount; i++)
+    {
+        if (LT[i].name == s)
+            return true;
+    }
     return false;
 }
 
-int getSymID(string s)
+int getlitid(string s)
 {
-    for (int i = 0; i < scnt; i++)
-        if (ST[i].name == s)
-            return i;
-    return -1;
-}
-
-bool presentLTCurrentPool(string s)
-{
-    for (int i = poolStartIndex; i < lcnt; i++)
-        if (LT[i].name == s)
-            return true;
-    return false;
-}
-
-int getLitID(string s)
-{
-    for (int i = 0; i < lcnt; i++)
+    for (int i = 0; i < lcount; i++)
+    {
         if (LT[i].name == s)
             return i;
+    }
     return -1;
 }
-
-bool isNumber(const string &s)
+bool isnumber(const string &s)
 {
     for (char c : s)
+    {
         if (!isdigit(c))
+        {
             return false;
-    return !s.empty();
+        }
+    }
+    return true;
 }
 
+unordered_map<string, int> REG = {{"AREG", 1}, {"BREG", 2}, {"CREG", 3}, {"DREG", 4}};
+unordered_map<string, int> Cond = {{"LT", 1}, {"LE", 2}, {"EQ", 3}, {"GT", 4}, {"GE", 5}, {"ANY", 6}};
+
+int LC = 0;
 int main()
 {
     ifstream fin("input.txt");
-    ofstream ic("ic.txt"), st("symtable.txt"), lt("littable.txt"), pt("pooltable.txt");
-
+    ofstream icin("ic.txt"), symin("symtable.txt"), litin("littable.txt"), ptin("pooltable.txt");
     string label, opcode, op1, op2;
-    int LC = 0;
-
     while (fin >> label >> opcode >> op1 >> op2)
     {
-        int id = getOP(opcode);
-        string IC = "", lc = "---";
+        string ic = "", lc = "---";
+        int id = getop(opcode);
+
         if (id == -1)
+        {
             continue;
-
-        IC = "(" + optab[id].mclass + "," + optab[id].mnemonic + ")";
-
-        // START
+        }
+        ic = "(" + optab[id].mclass + "," + optab[id].mnemonic + ")";
         if (opcode == "START")
         {
-            if (op1 != "NAN")
-            {
-                LC = stoi(op1);
-                IC += "(C," + op1 + ")";
-            }
-            ic << lc << "\t" << IC << endl;
+            ic += "(C," + op1 + ")";
+            icin << lc << "\t" << ic << endl;
+            LC = stoi(op1);
             continue;
         }
-
-        // Label handling
         if (label != "NAN")
         {
-            if (!presentST(label))
-                ST[scnt++] = {scnt, label, to_string(LC)};
+            if (presentST(label))
+            {
+                int symid = getsymid(label);
+                ST[symid].addr = LC;
+            }
             else
-                ST[getSymID(label)].addr = to_string(LC);
+            {
+                ST[scount] = {scount, label, to_string(LC)};
+                scount++;
+            }
         }
-
-        // ORIGIN
         if (opcode == "ORIGIN")
         {
             string token1, token2;
             char op;
 
-            int pos = op1.find('+'); // find only '+'
-            if (pos != -1)
-                op = '+';
-            else
+            int pos = op1.find('+');
+            if (pos == -1)
+            {
                 op = '-';
-
+            }
+            else
+            {
+                op = '+';
+            }
             stringstream ss(op1);
             getline(ss, token1, op);
             getline(ss, token2);
-
-            int symID = getSymID(token1);
-            int symAddr = stoi(ST[symID].addr);
-            int offset = stoi(token2);
-
+            int symid = getsymid(token1);
+            int addr = stoi(ST[symid].addr);
             if (op == '+')
-                LC = symAddr + offset;
+            {
+                LC = addr + stoi(token2);
+            }
             else
-                LC = symAddr - offset;
-
-            IC += "(S,0" + to_string(ST[symID].no) + ")" + op + token2;
-
-            ic << lc << "\t" << IC << endl;
-
+            {
+                LC = addr + stoi(token2);
+            }
+            ic += "(S," + to_string(ST[symid].no) + ")" + op + token2;
+            icin << lc << "\t" << ic << endl;
             continue;
         }
-
-        // EQU
         if (opcode == "EQU")
         {
-            int opid = getSymID(op1);
-            int labelid = getSymID(label);
+            int labelid = getsymid(label);
+            int opid = getsymid(op1);
             ST[labelid].addr = ST[opid].addr;
-            IC += "(S," + to_string(ST[opid].no) + ")";
-            ic << lc << "\t" << IC << endl;
+            ic += "(S," + to_string(ST[opid].no) + ")";
+            icin << lc << "\t" << ic << endl;
             continue;
         }
-
-        // LTORG / END
         if (opcode == "LTORG" || opcode == "END")
         {
-            ic << lc << "\t" << IC << endl;
-            for (int i = lcnt - nlcnt; i < lcnt; i++)
+            icin << lc << "\t" << ic << endl;
+            for (int i = lcount - nlcount; i < lcount; i++)
             {
-                lc = to_string(LC);
-                IC = "(DL,01)(C," + string(1, LT[i].name[2]) + ")";
-                LT[i].addr = to_string(LC++);
-                ic << lc << "\t" << IC << endl;
+                LT[i].addr = to_string(LC);
+                ic = "(DL,01)(C," + string(1, LT[i].name[2]) + ")";
+                icin << to_string(LC) << "\t" << ic << endl;
+                LC++;
             }
-            if (nlcnt > 0)
+            if (nlcount > 0)
             {
-                PT[pcnt] = {pcnt, "#" + to_string(LT[lcnt - nlcnt].no)};
-                pcnt++;
+                PT[pcount] = {pcount, "#" + to_string(LT[lcount - nlcount].no)};
+                nlcount = 0;
+                poolstartindex = lcount;
             }
-            nlcnt = 0;
-            poolStartIndex = lcnt;
             if (opcode == "END")
             {
                 break;
             }
             continue;
         }
-
         lc = to_string(LC);
-
-        // DS
         if (opcode == "DS")
         {
-            IC += "(C," + op1 + ")";
+            ic += "(C," + op1 + ")";
             LC += stoi(op1);
         }
-        // DC
         else if (opcode == "DC")
         {
-            IC += "(C," + op1 + ")";
+            ic += "(C," + op1 + ")";
             LC++;
         }
-        // Imperative Statements
         else if (optab[id].mclass == "IS")
         {
             if (opcode != "STOP")
             {
                 if (opcode == "BC")
-                    IC += "(" + to_string(cond[op1]) + ")";
-                else if (opcode == "READ" || opcode == "PRINT")
+                {
+                    ic += "(" + to_string(Cond[op1]) + ")";
+                }
+                else if (opcode == "PRINT" || opcode == "READ")
                 {
                     if (!presentST(op1))
-                        ST[scnt++] = {scnt, op1, "0"};
-                    IC += "(S,0" + to_string(getSymID(op1)) + ")";
+                    {
+                        ST[scount] = {scount, op1, "0"};
+                        scount++;
+                    }
+                    int symid = getsymid(op1);
+                    ic += "(S," + to_string(ST[symid].no) + ")";
                 }
-
                 else if (op1 != "NAN")
-                    IC += "(" + to_string(reg[op1]) + ")";
+                {
+                    ic += "(" + to_string(REG[op1]) + ")";
+                }
             }
             if (op2 != "NAN")
             {
-                if (op2[0] == '=')
+                if (op2.find('=') != string::npos)
                 {
-                    if (!presentLTCurrentPool(op2))
+                    if (!presentcurrentpool(op2))
                     {
-                        LT[lcnt++] = {lcnt, op2, "0"};
-                        nlcnt++;
+                        LT[lcount] = {lcount, op2, "0"};
+                        lcount++;
+                        nlcount++;
                     }
-                    IC += "(L,0" + to_string(getLitID(op2)) + ")";
+                    int litid = getlitid(op2);
+                    ic += "(L," + to_string(LT[litid].no) + ")";
                 }
-                else if (isNumber(op2))
+                else if (isnumber(op2))
                 {
-                    IC += "(C," + op2 + ")";
+                    ic += "(C," + op2 + ")";
                 }
                 else
                 {
                     if (!presentST(op2))
-                        ST[scnt++] = {scnt, op2, "0"};
-                    IC += "(S,0" + to_string(getSymID(op2)) + ")";
+                    {
+                        ST[scount] = {scount, op2, "0"};
+                        scount++;
+                    }
+                    int symid = getsymid(op2);
+                    ic += "(S," + to_string(ST[symid].no) + ")";
                 }
             }
             LC++;
         }
-
-        ic << lc << "\t" << IC << endl;
+        icin << lc << "\t" << ic << endl;
     }
-
-    for (int i = 0; i < scnt; i++)
-        st << ST[i].no << "\t" << ST[i].name << "\t" << ST[i].addr << endl;
-
-    for (int i = 0; i < lcnt; i++)
-        lt << LT[i].no << "\t" << LT[i].name << "\t" << LT[i].addr << endl;
-
-    for (int i = 0; i < pcnt; i++)
-        pt << PT[i].no << "\t" << PT[i].litno << endl;
-
+    for (int i = 0; i < scount; i++)
+    {
+        symin << ST[i].no << "\t" << ST[i].name << "\t" << ST[i].addr << endl;
+    }
+    for (int i = 0; i < lcount; i++)
+    {
+        litin << LT[i].no << "\t" << LT[i].name << "\t" << LT[i].addr << endl;
+    }
+    for (int i = 0; i < pcount; i++)
+    {
+        ptin << PT[i].no << "\t" << PT[i].literalno << endl;
+    }
     return 0;
 }
